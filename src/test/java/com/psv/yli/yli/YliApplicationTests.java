71888e436f096c311b3daabcec4d18b6234cef30
Vignoni Pablo <pablovignoni@gmail.com>
2022-01-15 07:45:06 -0300

    initial commit

---
+++ src/test/java/com/psv/yli/yli/YliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psv.yli.yli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
